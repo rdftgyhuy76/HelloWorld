4e602dfabbd4c8d7fee1178b1868870789bc60dd
rdftgyhuy76 <js1thegaker@hotmail.com>
2024-05-10 02:51:47 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const dropRightWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
-  return arr;
-};
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
