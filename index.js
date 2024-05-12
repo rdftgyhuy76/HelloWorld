fc98edbbb2e36a70f838a569a35f16e645c38c84
rdftgyhuy76 <js1thegaker@hotmail.com>
2024-05-12 14:08:13 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const uniqueValues = (arr) => [...new Set(arr)];
+function rotateRight(head, k) {
+  if (!head || k === 0) return head;
+  let length = 1;
+  let tail = head;
+  while (tail.next) {
+    length++;
+    tail = tail.next;
+  }
+  k = k % length;
+  if (k === 0) return head;
+  let newTail = head;
+  for (let i = 1; i < length - k; i++) newTail = newTail.next;
+  const newHead = newTail.next;
+  newTail.next = null;
+  tail.next = head;
+  return newHead;
+}
